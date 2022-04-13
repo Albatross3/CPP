0fd187cd576c38a83b00ef3fb33a7d5855fcc9ca
DongJunLee <superstring77@gmail.com>
2022-04-13 22:44:44 +0900

    basic

--- DataStructure/Basic/Basic/basic.cpp
+++ DataStructure/Basic/Basic/basic.cpp
@@ -1,6 +1,12 @@
 # include <iostream>
+#include <algorithm>
+#include <numeric>
+
+
 using namespace std;
 int main()
 {
-	
+	int a[] = { 1,2,3 };
+	int result = accumulate(a, a + 3, 1,multiplies<int>());
+	cout << result;
 }
